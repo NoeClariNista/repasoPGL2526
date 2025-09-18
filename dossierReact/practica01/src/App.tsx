78f64aab28db4bbda33096e5502c04eb9528a4e2
NoeClariNista <chispa1992@gmail.com>
2025-09-18 18:46:12 +0100

    feat(practica01): Se realiza la práctica 01 del Dossier de React de PGL.

---
+++ dossierReact/practica01/src/App.tsx
@@ -0,0 +1,15 @@
+/**
+* @author Noelia Hernández Domínguez.
+*/
+
+function App() {
+
+    return (
+        <div className="App">
+            <h3> ¡Hola Mundo!. </h3>
+            <h3> Noelia Hernández Domínguez. </h3>
+        </div>
+    );
+}
+
+export default App;
